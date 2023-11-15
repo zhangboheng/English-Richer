c61be754eddf275c30ef436135fbb4993a162177
zhangboheng <zhangboheng827@gmail.com>
2023-11-15 15:39:06 +0800

    初步实现初中等级的根据中文输入英文

--- EnglishRicher/pages/gametwo/junior/index.js
+++ EnglishRicher/pages/gametwo/junior/index.js
@@ -10,9 +10,9 @@ Page({
     showGrade: '初中'
   },
   // 页面分享
-  onShareAppMessage() {},
+  onShareAppMessage() { },
   // 页面分享朋友圈
-  onShareTimeline() {},
+  onShareTimeline() { },
   onLoad: function (options) {
     // 初次加载获取数据
     let defaultLevel = wx.getStorageSync('defaultLevel'); // 初始水平
@@ -33,46 +33,66 @@ Page({
       inputValue: event.detail.value
     });
   },
-  // 点击掌握后进行下一个单词
-  handleMaster: function() {
-    // 掌握一个增加1分钱
-    let money = wx.getStorageSync('money');
-    money = money + 0.01;
-    wx.setStorageSync('money', Number(money.toFixed(2)));
-    // 掌握一个增加1经验值
-    let getProgress = wx.getStorageSync('progress') || 0;
-    getProgress = getProgress + 0.1;
-    wx.setStorageSync('progress', Number(getProgress.toFixed(2)));
-    wx.setStorageSync('startGrade', Math.floor(getProgress/100));
-    // 显示增加分数的悬浮动画
-    this.setData({
-      showAnimation: true,
-    });
-    const that = this;
-    setTimeout(function () {
-      that.setData({
-        showAnimation: false,
+  // 点击确认后进行下一个单词
+  handleMaster: function () {
+    let getInputWord = this.data.inputValue.toLowerCase().trim();
+    if (getInputWord == this.data.word.toLowerCase()) {
+      // 掌握一个增加1分钱
+      let money = wx.getStorageSync('money');
+      money = money + 0.01;
+      wx.setStorageSync('money', Number(money.toFixed(2)));
+      // 掌握一个增加1经验值
+      let getProgress = wx.getStorageSync('progress') || 0;
+      getProgress = getProgress + 0.1;
+      wx.setStorageSync('progress', Number(getProgress.toFixed(2)));
+      wx.setStorageSync('startGrade', Math.floor(getProgress / 100));
+      // 显示增加分数的悬浮动画
+      this.setData({
+        inputValue: '',
+        showAnimation: true,
       });
-    }, 1000);
-    this.getNextWord();
+      const that = this;
+      setTimeout(function () {
+        that.setData({
+          showAnimation: false,
+        });
+      }, 1000);
+      wx.showToast({
+        title: '哎哟，不错哦，答对了！',
+        icon: 'none',
+        duration: 1000
+      }); 
+      this.getNextWord();
+    } else {
+      wx.showToast({
+        title: '哎哟，不好意思，答错了！',
+        icon: 'none',
+        duration: 1000
+      }); 
+    }
   },
 
-  handleNotMaster: function() {
+  handleNotMaster: function () {
     // 将单词缓存到本地数据
     let notMasterWords = wx.getStorageSync('notMasterWords') || [];
     // 如果缓存内没有则可以放置
-    if (notMasterWords.map(x=>x.word).indexOf(this.data.word)==-1) {
-      notMasterWords.push({word:this.data.word, translations:this.data.translations});
+    if (notMasterWords.map(x => x.word).indexOf(this.data.word) == -1) {
+      notMasterWords.push({ word: this.data.word, translations: this.data.translations });
     }
     wx.setStorageSync('notMasterWords', notMasterWords);
+    wx.showToast({
+      title: `正确答案是${this.data.word}, 已经加入到温故知新！`,
+      icon: 'none',
+      duration: 2000
+    }); 
     this.getNextWord();
   },
 
-  getNextWord: function() {
+  getNextWord: function () {
     // 生成0到1990之间的随机数
     const randomNum = Math.floor(Math.random() * this.data.listData.length);
     this.setData({
-      translationShow: false,
+      inputValue: '', // 输入框的值
       word: this.data.listData[randomNum].word,
       translations: this.data.listData[randomNum].translations,
     });
