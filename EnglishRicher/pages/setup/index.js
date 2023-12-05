e765345748031a3ff49e0c37e3c755a63a8f3e5a
zhangboheng <zhangboheng827@gmail.com>
2023-12-05 22:14:39 +0800

    修复初始页面随机音频彩蛋在 Android 和 Windows 上没有声音问题

--- EnglishRicher/pages/setup/index.js
+++ EnglishRicher/pages/setup/index.js
@@ -1,10 +1,10 @@
 const innerAudioContext = wx.createInnerAudioContext({
   useWebAudioImplement: false
 });
-var videoContext = wx.createVideoContext('myVideo', this);  // 获取视频上下
+var videoContext = ''  // 获取视频上下
 Page({
   data: {
-    randomText: 'V2.2.3 发布，新增伴音彩蛋，首页按住 Logo 顺时针旋转一周，三次即可解锁，点击 Logo 可暂停～～',
+    randomText: 'V2.2.4 发布，修复初始页面随机音频彩蛋在 Android 和 Windows 上没有声音问题～～',
     textCollection: [
       '使用小技巧，开启旅途中的小齿轮点击后可以进入策划谋略页面哦～～',
       '想要获取更多的经验和钱币？可以通过交换市集、关卡和任务大厅获取哦～～',
@@ -12,10 +12,10 @@ Page({
       '解限卡可将温故知新容量扩充为2000，可以在兑换市集兑换～～',
       '多利用温故知新可以帮助你更快掌握不会的单词哦～～',
       '开启旅途新增经验学堂，分享学习上的经验技巧～～',
-      'V2.2.3 发布，新增伴音彩蛋，首页按住 Logo 顺时针旋转一周，三次即可解锁，点击 Logo 可暂停～～'
+      '伴音彩蛋，首页按住 Logo 顺时针旋转一周，三次即可解锁，点击 Logo 可停止～～'
     ],
     showComponent: false,
-    showTips: "\n英语大富翁主旨是辅佐用户学习英语，通过一个个关卡，寓教于乐，提高了英语水平，收获了财富信息。\n\n1.点击开启旅途后，可以进入正式故事，先设置名字，初始等级和财富都是0，后续随着掌握的单词越多，等级和财富自然就随之变化。\n\n2.等级是通往下一关卡的重要凭据，而财富则是用来在兑换市集用来兑换各种实用情报，比如彩票，经验卡，考试资料，英语字幕，话费优惠信息，优惠电影票，满减商品等。\n\n3.获取经验和钱币的方式除了通过刷单词外，还可以通过兑换市集以及任务大厅获取。\n\n4.英语水平目前包括小学、初中、高中、大学英语四级、大学英语六级、考研、托福和 SAT，修改适配水平请前往策划谋略页面。\n\n5.一切数据都保存在本机，如果清空数据后，所有数据也会清除。\n\n",
+    showTips: "\n英语大富翁主旨是辅佐用户学习英语，通过一个个关卡，寓教于乐，提高了英语水平，收获了财富信息。\n\n1.点击开启旅途后，可以进入正式故事，先设置名字，初始等级和财富都是0，后续随着掌握的单词越多，等级和财富自然就随之变化。\n\n2.等级是通往下一关卡的重要凭据，而财富则是用来在兑换市集用来兑换各种实用情报，比如彩票，经验卡，考试资料，英语字幕，话费优惠信息，优惠电影票，满减商品等。\n\n3.获取经验和钱币的方式除了通过刷单词外，还可以通过兑换市集以及任务大厅获取。\n\n4.英语水平目前包括小学、初中、高中、大学英语四级、大学英语六级、考研、托福和 SAT，修改适配水平请前往策划谋略页面。\n\n5.伴音彩蛋，首页按住 Logo 顺时针旋转一周，三次即可解锁，点击 Logo 可停止。\n\n6.一切数据都保存在本机，如果清空数据后，所有数据也会清除。\n\n",
     startX: 0,
     startY: 0,
     rotate: 0,
@@ -94,6 +94,7 @@ Page({
   // 触摸结束事件
   onTouchEnd: function () {
     // 检查是否完成三圈旋转
+    videoContext = wx.createVideoContext('myVideo', this);
     var rotationCount = this.data.rotationCount;
     var rotate = this.data.rotate;
     var that = this;
@@ -104,11 +105,13 @@ Page({
       wx.getSystemInfo({
         success: function(res) {
           if (res.platform === 'ios') {
+            innerAudioContext.stop();
             innerAudioContext.play();
           } else {
             that.setData({
               musicLink: links
             })
+            videoContext.pause();
             videoContext.play();
           }
         }
