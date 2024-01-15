aa2c81176700115327b33889fe64588e6b99e517
zhangboheng <zhangboheng827@gmail.com>
2024-01-15 11:42:10 +0800

    新增高中水平单词音标显示

--- EnglishRicher/pages/high/index/index.js
+++ EnglishRicher/pages/high/index/index.js
@@ -14,7 +14,7 @@ Page({
     wx.showToast({
       title: '努力加载中……',
       icon: 'loading',
-      duration: 500
+      duration: 3000
     });
     // 初次加载获取数据
     let trueData = database.postData.main.sort(this.shuffleArray);
--- EnglishRicher/pages/junior/index/index.js
+++ EnglishRicher/pages/junior/index/index.js
@@ -14,7 +14,7 @@ Page({
     wx.showToast({
       title: '努力加载中……',
       icon: 'loading',
-      duration: 500
+      duration: 1500
     });
     // 初次加载获取数据
     let trueData = database.postData.main.sort(this.shuffleArray);
